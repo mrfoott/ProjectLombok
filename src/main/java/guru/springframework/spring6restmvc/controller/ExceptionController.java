8a1527e073d48f22ed0f397ae5a9d01319546856
mrfoott <phuvo2105@gmail.com>
2024-01-03 17:00:34 +0700

    Use annotation ResponseStatus instead of ControllerAdvice

--- src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
@@ -4,14 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+//@ControllerAdvice
 // Add annotation @ControllerAdvice will allow to have a global exception handler and handle it globally!!!
-public class ExceptionController {
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity handleNotFoundException() {
-        System.out.println("Global exception handler!!!");
-        System.out.println("ERROR!!!\n404 NOT FOUND");
-
-        return ResponseEntity.notFound().build();
-    }
-}
+//public class ExceptionController {
+//    @ExceptionHandler(NotFoundException.class)
+//    public ResponseEntity handleNotFoundException() {
+//        System.out.println("Global exception handler!!!");
+//        System.out.println("ERROR!!!\n404 NOT FOUND");
+//
+//        return ResponseEntity.notFound().build();
+//    }
+//}
