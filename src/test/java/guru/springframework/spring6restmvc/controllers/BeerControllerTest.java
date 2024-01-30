657709795b10005d8ff16cded9bc829e75f81455
mrfoott <phuvo2105@gmail.com>
2024-01-30 16:58:47 +0700

    Fix get beer list

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
@@ -192,7 +192,7 @@ class BeerControllerTest {
     @Test
     void testListBeers() throws Exception {
 
-        given(beerService.listBeers(null, null, false)).willReturn(beerServiceImpl.listBeers(null, null, false));
+        given(beerService.listBeers(any(), any(), any())).willReturn(beerServiceImpl.listBeers(null, null, false));
 
         mockMvc.perform(get(BeerController.BEER_PATH)
                         .accept(MediaType.APPLICATION_JSON))
@@ -214,7 +214,7 @@ class BeerControllerTest {
 
     @Test
     void getBeerListNotFound() throws Exception {
-        given(beerService.listBeers(null, null, false)).willThrow(NotFoundException.class);
+        given(beerService.listBeers(any(), any(), any())).willThrow(NotFoundException.class);
 
         mockMvc.perform(get(BeerController.BEER_PATH))
                 .andExpect(status().isNotFound());
